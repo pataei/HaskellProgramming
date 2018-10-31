1e74566cbae5aa58d4f2bc3e605030c34bd0f519
Parisa Ataei <>
2018-10-31 16:26:35 -0700

    ch6 done

--- ch6.hs
+++ ch6.hs
@@ -1,5 +1,7 @@
 module Ch6 where
 
+import Data.List
+
 data TisAnInteger = TisAn Integer 
 
 instance Eq TisAnInteger where 
@@ -30,3 +32,29 @@ instance (Eq a, Eq b) => Eq (EitherOr a b) where
   (==) (Hello x) (Hello x') = x==x'
   (==) (Bye y)   (Bye y')   = y==y'
   (==) _ _ = False
+
+i :: RealFrac a => a 
+i = 1.0
+
+freud :: a -> a 
+freud x = x
+
+myX = 1 :: Int
+sigmund :: Int -> Int
+sigmund x = myX
+
+jung :: Ord a => [a] -> a
+jung xs = head (sort xs)
+
+mySort :: [Char] -> [Char]
+mySort = sort
+
+signifier :: [Char] -> Char
+signifier xs = head (mySort xs)
+
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith f x y = (f y) + fromInteger x
+
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f x y = (f x) == y
