d979b20aca1147597239e1e87152efaf55a82803
Parisa Ataei <>
2018-11-05 17:00:58 -0800

    ch7 problems, couldn't get the last one ...

---
+++ Ch7.hs
@@ -0,0 +1,34 @@
+module Ch7 where
+
+tensDigit :: Integral a => a -> a 
+tensDigit x = d
+  where xLast = x `div` 10
+        d = xLast `mod` 10
+
+tensDigit' :: Integral a => a -> a
+tensDigit' x = snd $ divMod (fst (divMod x 10)) 10
+
+hunsDigit' :: Integral a => a -> a
+hunsDigit' x = snd $ divMod (fst (divMod x 100)) 100
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y b 
+  | b==False = x
+  | b==True = y
+
+foldBool' :: a -> a -> Bool -> a
+foldBool' x y b = case b of 
+                    False -> x
+                    True -> y
+
+g :: (a -> b) -> (a,c) -> (b,c)
+g f (a,c) = (f a, c)
+
+roundTrip :: (Show a, Read a) => a -> a
+roundTrip a = read (show a)
+
+roundTrip' :: (Show a, Read a) => a -> a
+roundTrip' = read . show 
+
+roundTrip'' :: (Show a, Read b) => a -> b
+roundTrip'' x = read ((show x) :: String)
